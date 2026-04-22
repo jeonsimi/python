ef93ea4273cda0a3f3e6c632721659eba2fffe06
swarnakanya bose <swarnakanyabose@gmail.com>
2026-04-22 18:06:19 +0530

    mark programe

---
+++ mark.py
@@ -0,0 +1,8 @@
+#by default python takes user input as string 
+d = input("enter a number: ")
+print (type(d))
+
+#to take any oter type of input it has to be 
+#put as below
+f = int (input("enter a number:"))
+print(type(f))
