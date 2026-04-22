6aff3a73e614e90d4d9e901f98ca489eb15f1b27
swarnakanya bose <swarnakanyabose@gmail.com>
2026-04-22 19:43:20 +0530

    largest programe

---
+++ largest.py
@@ -0,0 +1,6 @@
+num1= int (input("enter the first number: "))
+num2= int (input("enter the second number: "))
+if num1>num2:
+    print(num1,"is the largest number")
+else:
+    print(num2,"is the largest number")
