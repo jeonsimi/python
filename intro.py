62ba0db003d932f9cce9a97aba21b6dd747f56fb
swarnakanya bose <swarnakanyabose@gmail.com>
2026-04-23 19:41:10 +0530

    intro programe

---
+++ intro.py
@@ -0,0 +1,10 @@
+name ="swarnakanya bose"
+subject = "computer"
+age = 21
+print(age)
+print (type(age))
+print (" my name is {} and i like {} I am {} year old,". format (name , subject , age))
+print ("i am {2} year old, my name is {0} and i like {1}". format (name, subject, age))
+print (f"my name is {name} my favourite subject is {subject}and i am {age} years old")
+age = "apple"
+print(age)
